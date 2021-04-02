ca2e33f29d1aa9ded2ef69e11d29351ea99dbdeb
Mark <mark.young@cuanswers.com>
2021-04-02 05:57:47 -0400

    Add a little html to return

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from datetime import datetime, date, time
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "This is awesome!"
+    user = {'username': 'Mark'}
+    currentDate = datetime.now()
+    today = datetime.date(currentDate).strftime("%d-%b-%Y")
+    currentTime = datetime.time(currentDate).strftime("%H:%M:%S")
+    return '''
+<html>
+<head>
+<title>Home Page - Microblog</title>
+</head>
+<body>
+<h1>''' + today + '''</h1>
+<h2>Hello, ''' + user['username'] + '''! The time is ''' + currentTime + '''</h2>
+</body>
+</html>'''
